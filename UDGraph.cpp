d01e546108bc829c1b41ef31127c5785aecff2ef
Geed-wei <57037616+Geed-wei0111@users.noreply.github.com>
2019-10-28 22:37:56 +0800

    UDGraph.cpp
    NEW!!!

--- UDGraph.cpp
+++ UDGraph.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <string>
+#include <queue>
 
 const int MAXSIZE = 10;
 class UDGraph
@@ -37,6 +38,7 @@ public:
 				}
 			}
 		}
+		system("CLS");
 	}
 	void PrintGraphMatrix()
 	{
@@ -73,7 +75,96 @@ public:
 		i = SearchArcs(i, j);
 		return i;
 	}
+	int GetDegree(int pos)
+	{
+		int i, count = 0;
+		for (i=0;i<Count;i++)
+		{
+			if ( this->Arcs[pos][i] == 1 )
+				count++;
+		}
+		return count;
+	}
+	int GetDegree()
+	{
+		int pos;
+		std::cout << "Pos:";
+		std::cin >> pos;
+		if ( pos >= 0 )
+		{
+			return GetDegree(pos);
+		}
+		else
+			return -1;
+	}
+
+	void BFStraverse()
+	{
+		std::cout << "BFS:" ;
+		int i, j;
+		int *visited = new int[Count];
+		memset(visited, 0, sizeof(int)*Count);
+		std::queue<std::string> Q;
+		for (i=0;i<Count;i++)
+		{
+			if ( !visited[i] )
+			{
+				std::cout << this->vertex[i] << ' ';
+				visited[i] = 1;
+				Q.push(this->vertex[i]);
+
+				while ( !Q.empty() )
+				{
+					std::string temp = Q.front();
+					Q.pop();
+					int t = temp[1] - '0';
+					for (j=0;j<Count;j++)
+					{
+						if ( this->Arcs[i][j] == 1 && !visited[i] )
+						{
+							std::cout << this->vertex[i] << ' ';
+							visited[i] = 1;
+							Q.push(this->vertex[i]);
+						}
+					}
+				}
+			}
+		}
+		delete visited;
+		std::cout << std::endl;
+		return ;
+	}
+	void DFStraverse()
+	{
+		int *visited = new int[Count];
+		memset(visited, 0, sizeof(int)*Count);
+		if ( Count != 1 )
+		{
+			std::cout << "DFS:" << this->vertex[0] << ' ';
+			visited[0] = 1;
+			DFStraverse(visited, 0);
+		}
+		else
+		{
+			std::cout <<  "DFS:" << this->vertex[0] << ' ';
+		}
+		std::cout << std::endl;
+		delete visited;
+	}
 private:
+	void DFStraverse(int *visited, int pos)
+	{
+		int i;
+		for (i=0;i<Count;i++)
+		{
+			if ( this->Arcs[pos][i] == 1 && !visited[i])
+			{
+				std::cout << this->vertex[i] << ' ';
+				visited[i] = 1;
+				DFStraverse(visited, i);
+			}
+		}
+	}
 	int Count;
 	std::string vertex[MAXSIZE];
 	int Arcs[MAXSIZE][MAXSIZE];
@@ -83,5 +174,7 @@ int main()
 {
 	UDGraph graph;
 	graph.PrintGraphMatrix();
+	graph.BFStraverse();
+	graph.DFStraverse();
 	return 0;
 }
